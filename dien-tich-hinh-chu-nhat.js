5698ae70322de6e3c6d14ecd44b849142ac56f24
tranquocsang <tranquocsang19977@gmail.com>
2019-06-24 06:30:24 +0700

    baitap

---
+++ dien-tich-hinh-chu-nhat.js
@@ -0,0 +1,8 @@
+let inputWidth
+let inputHeight
+inputWidth = prompt("nhập vào độ rộng")
+inputHeight = prompt("nhập vào chiều cao")
+let width = parseInt(inputHeight)
+let height = parseInt(inputWidth)
+let area = width * height
+document.write(" diện tích " + area)
